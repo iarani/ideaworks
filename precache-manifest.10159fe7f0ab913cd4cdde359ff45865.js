057f03b12abadc66f049785f3de5a9c5aa549ce0
arani-MacBookPro <53565610+iarani1975@users.noreply.github.com>
2020-02-26 09:51:14 +0530

    Updates

---
+++ precache-manifest.10159fe7f0ab913cd4cdde359ff45865.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d46a82c3dac3cb49788c3a58556250d4",
+    "url": "/ideaworks/index.html"
+  },
+  {
+    "revision": "4f4fa8a2e23b6b7af9be",
+    "url": "/ideaworks/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "c66573611743c07fabb0",
+    "url": "/ideaworks/static/css/main.2111bbca.chunk.css"
+  },
+  {
+    "revision": "4f4fa8a2e23b6b7af9be",
+    "url": "/ideaworks/static/js/2.26302d95.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/ideaworks/static/js/2.26302d95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c66573611743c07fabb0",
+    "url": "/ideaworks/static/js/main.c362ca48.chunk.js"
+  },
+  {
+    "revision": "9e4a7f64b5d6bbf60cdd",
+    "url": "/ideaworks/static/js/runtime-main.b9052afc.js"
+  },
+  {
+    "revision": "23516a467070a044cffbdb1ef5019c9b",
+    "url": "/ideaworks/static/media/Indiafrica-01.23516a46.jpg"
+  },
+  {
+    "revision": "ed5129e5eb0cf05aedf976787083d4b1",
+    "url": "/ideaworks/static/media/Indiafrica-02.ed5129e5.jpg"
+  },
+  {
+    "revision": "0eb8a631c8e0a1526c41147e2d200d93",
+    "url": "/ideaworks/static/media/Indiafrica-03.0eb8a631.jpg"
+  },
+  {
+    "revision": "7706b6e6a5fd3c4d42e4cad84e2c36c2",
+    "url": "/ideaworks/static/media/Indiafrica-04.7706b6e6.jpg"
+  },
+  {
+    "revision": "1dff5568273416433881a00f1d669e91",
+    "url": "/ideaworks/static/media/Indiafrica-05.1dff5568.jpg"
+  },
+  {
+    "revision": "9e9adb433b4bba3dbe887aaf54ba4948",
+    "url": "/ideaworks/static/media/Indiafrica-06.9e9adb43.jpg"
+  },
+  {
+    "revision": "a5c5b35c547b446bb22aafbf50e1eb9d",
+    "url": "/ideaworks/static/media/Indiafrica-07.a5c5b35c.jpg"
+  },
+  {
+    "revision": "4cf9c5ec533d8587ee5e0145d171264f",
+    "url": "/ideaworks/static/media/Indiafrica-08.4cf9c5ec.jpg"
+  },
+  {
+    "revision": "c85b07222f01cc448d29e9e81abf5642",
+    "url": "/ideaworks/static/media/Indiafrica-09.c85b0722.jpg"
+  },
+  {
+    "revision": "1156896f47e15048234d4534fd238621",
+    "url": "/ideaworks/static/media/Indiafrica-10.1156896f.jpg"
+  },
+  {
+    "revision": "049e3ea57dbbb94fdb4a5c8b331142a2",
+    "url": "/ideaworks/static/media/Indiafrica-11.049e3ea5.jpg"
+  },
+  {
+    "revision": "e426377aa9881fae41b3c5aad580ff5e",
+    "url": "/ideaworks/static/media/Indiafrica-12.e426377a.jpg"
+  },
+  {
+    "revision": "1d43fc89e8f6ae02c337be52e4540818",
+    "url": "/ideaworks/static/media/Indiafrica-13.1d43fc89.jpg"
+  },
+  {
+    "revision": "d420f6f1e398003f15e45855c42560b6",
+    "url": "/ideaworks/static/media/Indiafrica-14.d420f6f1.jpg"
+  },
+  {
+    "revision": "eeadc16a7f1e2f60c6c2282fd907eeec",
+    "url": "/ideaworks/static/media/Indiafrica-15.eeadc16a.jpg"
+  },
+  {
+    "revision": "66f4ecdb36cdc74ac2a7fb345ad30866",
+    "url": "/ideaworks/static/media/Indiafrica-16.66f4ecdb.jpg"
+  },
+  {
+    "revision": "8e049a7b714037ebe95931b502c5beca",
+    "url": "/ideaworks/static/media/Indiafrica-17.8e049a7b.jpg"
+  },
+  {
+    "revision": "07de7f9964edbb54da41ac740a44562e",
+    "url": "/ideaworks/static/media/Indiafrica-18.07de7f99.jpg"
+  },
+  {
+    "revision": "0a86b36285983d0fb5a04ac6803e8a42",
+    "url": "/ideaworks/static/media/Indiafrica-19.0a86b362.jpg"
+  },
+  {
+    "revision": "bc9247649b963d0b6f4def07c7bb8a55",
+    "url": "/ideaworks/static/media/Indiafrica-20.bc924764.jpg"
+  },
+  {
+    "revision": "fdf3d45f2dde7196ab8d06b87156f90f",
+    "url": "/ideaworks/static/media/Indiafrica-21.fdf3d45f.jpg"
+  },
+  {
+    "revision": "022e1f30d68241d9794655793e12f920",
+    "url": "/ideaworks/static/media/Indiafrica-22.022e1f30.jpg"
+  },
+  {
+    "revision": "6e36d5ed135d0a38b48526ee4f89372f",
+    "url": "/ideaworks/static/media/Indiafrica-23.6e36d5ed.jpg"
+  },
+  {
+    "revision": "9c9d695abef020f950dc20a14c9eff81",
+    "url": "/ideaworks/static/media/Indiafrica-24.9c9d695a.jpg"
+  },
+  {
+    "revision": "7538abc28d7d302cab4eabff73921592",
+    "url": "/ideaworks/static/media/Indiafrica-25.7538abc2.jpg"
+  },
+  {
+    "revision": "54a6a7b468e820ba73d0601e4528dd63",
+    "url": "/ideaworks/static/media/Indiafrica-26.54a6a7b4.jpg"
+  },
+  {
+    "revision": "3e26e2a6698b6047f2ebea0c1ba83c7c",
+    "url": "/ideaworks/static/media/Indiafrica-27.3e26e2a6.jpg"
+  },
+  {
+    "revision": "137de67e55e0666b8e0b63ee3fc5b753",
+    "url": "/ideaworks/static/media/davosvid.137de67e.webm"
+  },
+  {
+    "revision": "9c73aeca6aa9b935efdc6191c483e2fb",
+    "url": "/ideaworks/static/media/indiafrica_cover2.9c73aeca.jpg"
+  }
+]);
